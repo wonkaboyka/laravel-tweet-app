8f30b2921e48fd77108d3223bcb3a4edd1923541
vunguyenhp <vunguyenhp@Wonka-Boyka.localdomain>
2024-01-27 16:17:46 +0900

    01/27

--- resources/views/components/element/button-a.blade.php
+++ resources/views/components/element/button-a.blade.php
@@ -1,18 +1,26 @@
 @props([
-    'href'=>'',
-    'theme'=>'primary',
+    'href' => '',
+    'theme' => 'primary',
 ])
 @php
     if(!function_exists('getThemeClassForButtonA')){
-        function getThemeClassForButtonA($theme){
-            return match($theme){
-                'primary' => 'text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500',
-                'secondary' => 'text-white bg-orange-200 hover:bg-orange-300 focus:ring-red-500',
-                default=>' ',
-            };
+      function getThemeClassForButtonA($theme) {
+          return match ($theme) {
+              'primary' => 'text-white bg-blue-500 hover:bg-blue-600 focus:ring-blue-500',
+              'secondary' => 'text-white bg-red-500 hover:bg-red-600 focus:ring-red-500',
+              default => '',
+          };
         }
     }
 @endphp
-<a href="{{$href}}" class="inline-flex justify-center py-2 px-4 border-transparent shadow-sm text-sm font-medium rounded-md b text-white hover:bg-blue-600 bg-blue-500 focus:outline-none focus:ring-2 {{getThemeClassForButtonA($theme)}}" >
-    {{$slot}}
+<a href="{{ $href }}" class="
+    inline-flex justify-center
+    py-2 px-4
+    border border-transparent
+    shadow-sm
+    text-sm
+    font-medium
+    rounded-md
+    focus:outline-none focus:ring-2 focus:ring-offset-2 {{ getThemeClassForButtonA($theme) }}">
+    {{ $slot }}
 </a>
--- resources/views/components/tweet/form/post.blade.php
+++ resources/views/components/tweet/form/post.blade.php
@@ -20,17 +20,17 @@
         @enderror
         <div class="flex flexwrap justify-end">
             <x-element.button>
-                Ấn nút
+                Send
             </x-element.button>
         </div>
     </form>
 </div>
 @endauth
 @guest
-    <div class="flex flex-wrap justify-center">
-        <div class="w-1/2 p-4 flex flex-wrap justify-evenly">
-            <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
-            <x-element.button-a :href="route('register')" theme="secondary">会員登録</x-element.button-a>
-        </div>
+<div class="flex flex-wrap justify-center">
+    <div class="w-1/2 p-4 flex flex-wrap justify-evenly">
+        <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
+        <x-element.button-a :href="route('register')" theme="secondary">会員登録</x-element.button-a>
     </div>
+</div>
 @endguest
